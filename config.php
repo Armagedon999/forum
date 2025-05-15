b586d107b88a395d556d8f80540285c183aa7e42
irfa117 <irfanasome@gmail.com>
2025-05-15 10:08:16 +0700

    Initial commit

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php return array (
+  'debug' => false,
+  'database' => 
+  array (
+    'driver' => 'mysql',
+    'host' => '127.0.0.1',
+    'port' => 3307,
+    'database' => 'flarum',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => '',
+    'strict' => false,
+    'engine' => 'InnoDB',
+    'prefix_indexes' => true,
+  ),
+  'url' => 'http://localhost/forum/public',
+  'paths' => 
+  array (
+    'api' => 'api',
+    'admin' => 'admin',
+  ),
+  'headers' => 
+  array (
+    'poweredByHeader' => true,
+    'referrerPolicy' => 'same-origin',
+  ),
+  
+);
